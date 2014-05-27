96736e2056ca8de6fd9e1e92f7c1404d53a0a84b
Faezrah Rizalman <fwrizalman@gmail.com>
2014-05-27 22:00:58 +0100

    Docking Station can accept a bike

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,16 @@
+require 'docking_station'
+
+describe DockingStation do
+
+	let(:bike) { Bike.new }
+	let(:station) { DockingStation.new }
+	
+	it 'should accept a bike' do
+		expect(station.bike_count).to eq (0)
+
+		station.dock(bike)
+
+		expect(station.bike_count).to eq (1)
+	end
+
+end
