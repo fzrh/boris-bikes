9c3f35cb7a67f33242601bf72caf780414c8fd0b
Faezrah Rizalman <fwrizalman@gmail.com>
2014-05-28 21:07:45 +0100

    Extracted comman functionalities into BikeContainer module

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,45 +1,10 @@
-require 'docking_station'
+require './lib/docking_station'
 
 describe DockingStation do
+	let(:station) {DockingStation.new(:capacity => 123)}
 
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new(:capacity => 20) }
-
-	def fill_(station)
-		20.times { station.dock(Bike.new) }
-	end
-	
-	it 'should accept a bike' do
-		expect(station.bike_count).to eq (0)
-
-		station.dock(bike)
-
-		expect(station.bike_count).to eq (1)
-	end
-
-	it 'should release a bike' do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq (0)
-	end
-
-	it 'should know when it is full' do
-		expect(station).not_to be_full
-		fill_ station
-		expect(station).to be_full
-	end
-
-	it 'should not accept a bike if it is full' do
-		fill_ station
-		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
-	end
-
-	it 'should provide the list of available bikes' do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break!
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([working_bike])
+	it 'should allow setting default capacity when initialising' do
+		expect(station.capacity).to eq(123)
 	end
 
 end
