c9ed09eb78fd2e8d38a3b5479038fbd2b61c3daf
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-17 22:09:59 +0100

    Completed Van class

--- lib/van.rb
+++ lib/van.rb
@@ -1,17 +1,20 @@
-#Load BikeContainer
 require_relative 'bike_container'
 
 class Van
 
-	#this gives us all the methods that used to be in this class
 	include BikeContainer
 
 	def initialize(options = {})
-		# self.capacity is calling the capacity=() method
-		# (note the equals sign) defined in BikeContainer
-		# capacity (the second argument to fetch()) is calling
-		# the capacity() method in BikeContainer
+		@bikes = []
 		self.capacity = options.fetch(:capacity, capacity)
+	end
+
+	def collect_broken_bikes_from station
+		@bikes << station.release
+	end
+
+	def return_fixed_bikes_to station
+		station.accept
 	end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,10 +1,23 @@
-require './lib/van'
+require 'van'
+require 'docking_station'
 
 describe Van do
-	let(:van) {Van.new(:capacity => 30)}
+
+	let(:van) { Van.new(:capacity => 30) }
+	let(:station) { DockingStation.new }
 
 	it 'should allow setting default capacity when initialising' do
 		expect(van.capacity).to eq(30)
+	end
+
+	it 'should collect broken bikes from station' do
+		expect(station).to receive (:release)
+		van.collect_broken_bikes_from station
+	end
+
+	it 'should return fixed bikes to station' do
+		expect(station).to receive (:accept)
+		van.return_fixed_bikes_to station
 	end
 
 end
