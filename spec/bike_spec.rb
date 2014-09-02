dc87b3c44375f7170d2710e33e54fed8bf9e2c98
Faezrah Rizalman <fwrizalman@gmail.com>
2014-09-02 14:25:31 +0800

    Updated Bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,19 +4,17 @@ describe Bike do
 
 	let(:bike) { Bike.new }
 	
-	it 'should not be broken after we create it' do
+	it 'is not broken when created' do
 		expect(bike).not_to be_broken
 	end
 
 	it 'should be able to break' do
-		bike.break!
-		expect(bike).to be_broken
+		expect(bike.break!).to be_broken
 	end
 
 	it 'should be able to get fixed' do
 		bike.break!
-		bike.fix!
-		expect(bike).not_to be_broken
+		expect(bike.fix!).not_to be_broken
 	end
 
 end
